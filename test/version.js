35cf573cc18dab783b02db3fbe0f57ace0976064
Robert Curran <ro5635@gmail.com>
2019-02-24 23:09:01 +0000

    Added basic test example to check API version  Added basic circleci config file to run tests as part of a  docker-compose environment
     On branch master
    
     Changes to be committed:
    	new file:   .circleci/config.yml
    	new file:   test/version.js

---
+++ test/version.js
@@ -0,0 +1,48 @@
+/**
+ * Version test
+ *
+ * Basic unit tests for the version resource
+ */
+
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../app');
+let should = chai.should();
+
+const apiVersion = require('../package').version;
+
+chai.use(chaiHttp);
+
+describe('API Root', () => {
+    beforeEach((done) => {
+        done();
+
+    });
+
+    /*
+      * Test the /GET route
+      */
+    describe('/GET /', () => {
+        it('it should GET API details', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    // Expected response
+                    const expectedResponse = {msg: 'Service API', version: apiVersion};
+                    res.should.have.status(200);
+                    res.body.should.be.a('Object');
+                    res.body.should.have.property('version');
+                    res.body.should.have.property('msg').eql('Service API');
+                    // Duplicate tests a bit to demo the syntax options for future reference
+                    res.body.should.have.property('version').eql(expectedResponse.version);
+                    res.body.should.have.property('msg').eql(expectedResponse.msg);
+                    done();
+                });
+        });
+    });
+
+
+});
